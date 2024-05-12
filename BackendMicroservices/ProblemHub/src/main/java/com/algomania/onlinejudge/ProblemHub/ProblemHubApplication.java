3c13a48dd23b4e708d14c561e45fa3591ced1b25
Rishubh Kumar <82128737+Rishubh03@users.noreply.github.com>
2024-05-12 13:09:05 +0530

    First Commit for ProblemHub Microservice

---
+++ BackendMicroservices/ProblemHub/src/main/java/com/algomania/onlinejudge/ProblemHub/ProblemHubApplication.java
@@ -0,0 +1,13 @@
+package com.algomania.onlinejudge.ProblemHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProblemHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProblemHubApplication.class, args);
+	}
+
+}
